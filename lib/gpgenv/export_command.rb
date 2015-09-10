a67f578faba9386fd3b918d538bdc0831c93019e
Mike Shea <michael.shea@heroku.com>
2015-09-10 09:11:41 -0400

    Add export/import commands to/from .env

--- lib/gpgenv/export_command.rb
+++ lib/gpgenv/export_command.rb
@@ -2,5 +2,26 @@ require 'clamp'
 
 module Gpgenv
   class ExportCommand < Clamp::Command
+    parameter 'DIRS ...', 'dirs', :attribute_name => :directories
+
+    option '--file', 'FILE', 'env file to read from', :default => '.env'
+
+    def full_dir
+      if ENV['GPGENV_HOME']
+        "#{ENV['GPGENV_HOME']}/#{dir}"
+      else
+        dir
+      end
+    end
+
+    def execute
+      hash = Gpgenv.read_files(directories)
+      str = ''
+      hash.each do |k,v|
+        str << "#{k}=#{v}"
+      end
+      File.write(file, str)
+    end
+
   end
 end
