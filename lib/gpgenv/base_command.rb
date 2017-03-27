e6840d51266dfed558457d5c0b635b8fd0f8c4be
Mike Shea <michael.shea@heroku.com>
2017-03-27 11:52:18 -0400

    Create default gpgenv dir if it doesn't exist

--- lib/gpgenv/base_command.rb
+++ lib/gpgenv/base_command.rb
@@ -1,11 +1,16 @@
 require 'clamp'
+require 'fileutils'
 class Gpgenv
   class BaseCommand < Clamp::Command
 
     option ['-d', '--dir'], "DIR", "Directory to read env files from", default: "./.gpgenv"
 
     def gpgenv
-      @gpgenv ||= Gpgenv.new(dir: dir) 
+      # Create a new empty directory if we're using the default.
+      if dir == './.gpgenv'
+        FileUtils.mkdir_p(dir)
+      end
+      @gpgenv ||= Gpgenv.new(dir: dir)
     end
 
   end
