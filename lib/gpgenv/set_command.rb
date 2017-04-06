f375b549777e41414a6f420d2b110953f61d5834
Mike Shea <michael.shea@heroku.com>
2017-04-06 15:10:09 -0400

    Fix gpgset, bump version

--- lib/gpgenv/set_command.rb
+++ lib/gpgenv/set_command.rb
@@ -3,20 +3,21 @@ require 'gpgenv'
 require 'gpgenv/base_command'
 
 class Gpgenv
-  class SetCommand < Gpgenv::BaseCommand 
+  class SetCommand < Gpgenv::BaseCommand
 
     parameter "ARGUMENTS ...", "arguments", :attribute_name => :args
 
     def execute
-      FileUtils.mkdir_p(Gpgenv.dir)
+      FileUtils.mkdir_p(gpgenv.dir)
       if args.size == 1
         gpgenv.set(args[0], STDIN.read)
       elsif args.size == 2
         gpgenv.set(args.first, args.last)
-      else 
+      else
         fail("Usage: gpgset KEY [VALUE]")
       end
     end
+
 
   end
 end
--- lib/gpgenv/version.rb
+++ lib/gpgenv/version.rb
@@ -1,3 +1,3 @@
 class Gpgenv
-  VERSION = "0.1.10"
+  VERSION = "0.1.11"
 end
